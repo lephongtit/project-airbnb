7e3535aed98a3ba58e72b8220caa2110834d2955
Lê Hoàng Thiên Phong <thienphong251199@gmail.com>
2019-12-11 08:53:42 +0700

    oke

---
+++ src/main/java/com/codegym/backend_airbnb/BackEndAirbnbApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.backend_airbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAirbnbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndAirbnbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/backend_airbnb/BackEndAirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.backend_airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAirbnbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
